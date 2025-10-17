dd2c1a84ee906afb72d581a647099a68164c50e6
Novel-OxO <novel0x0@gmail.com>
2025-10-17 09:13:05 +0900

    feat: 글로벌 Exception Filter 구현 및 적용 - #1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 
+import { GlobalExceptionFilter } from './api/support';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 글로벌 Exception Filter 등록
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
