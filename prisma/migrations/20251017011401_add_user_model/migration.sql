b742be61e8806372046cc53e1e5eb212b0d49806
Novel-OxO <novel0x0@gmail.com>
2025-10-17 10:37:13 +0900

    feat: User 메타데이터 스키마 및 마이그레이션 추가 - #3

--- prisma/migrations/20251017011401_add_user_model/migration.sql
+++ prisma/migrations/20251017011401_add_user_model/migration.sql
@@ -1,10 +1,9 @@
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
-    "nickname" TEXT,
-    "email" TEXT,
-    "email_verified" TIMESTAMP(3),
-    "hashed_password" TEXT,
+    "nickname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashed_password" TEXT NOT NULL,
     "profile_image_url" TEXT,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
