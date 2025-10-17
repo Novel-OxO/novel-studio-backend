cc17f8e14b2cc9b4c6659e4f11cef638ddda310f
Novel-OxO <novel0x0@gmail.com>
2025-10-17 09:16:11 +0900

    feat: 글로벌 ValidationPipe 설정 추가 - #1
    - class-validator, class-transformer 패키지 설치
    - ValidationPipe 전역 설정 (whitelist, transform 등)
    - main.ts에 setupGlobalPipes 함수 추가

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { GlobalExceptionFilter } from './api/support';
 import { AppModule } from './modules/app.module';
 
+function setupGlobalPipes(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+}
+
+function setupGlobalFilters(app: INestApplication) {
+  app.useGlobalFilters(new GlobalExceptionFilter());
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 글로벌 Exception Filter 등록
-  app.useGlobalFilters(new GlobalExceptionFilter());
+  setupGlobalPipes(app);
+  setupGlobalFilters(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
